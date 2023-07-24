d05e86d33845dd46a841bc052665be0efffab808
nasirDev <bhio231nasir@gmail.com>
2023-07-24 07:27:08 +0700

    feat: search news

--- pages/index.vue
+++ pages/index.vue
@@ -4,24 +4,40 @@ import type { NewsParams } from '~/types'
 const { listNews } = useApi()
 
 const params: NewsParams = reactive({
-  limit: 4,
   page: 1,
+  limit: 3,
   q: '',
   category_id: '',
 })
 
-const { data, isLoading } = useQuery({
+const { data, isLoading, refetch } = useQuery({
   queryKey: ['news', params.page],
   queryFn: () => listNews(params),
 })
 
+watch(() => params.q, async (val) => {
+  await handleSearch(val)
+})
+
 const dataResult = computed(() => data.value?.data)
+
+const handleSearch = useDebounceFn(async (query: string) => {
+  params.q = query
+  await refetch()
+}, 250)
 </script>
 
 <template>
   <div max-w-5xl mx-a>
     <h1 v-if="isLoading" text-3xl font-bold>Loading...</h1>
     <div v-else>
+      <input
+        v-model="params.q"
+        type="text"
+        p="x2 y2"
+        class="border border-emerald rounded border-solid outline-none"
+        placeholder="Pencarian"
+      />
       <template v-for="item in dataResult" :key="item.id">
         <h2 text="2xl emerald">
           <NuxtLink :to="`/${item.slug}`">
